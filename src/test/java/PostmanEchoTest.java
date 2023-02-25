b3b4d9a8dd902673546ce1585047bddd12fc04a3
Oleg Sidorov <sidoroffoleg@mail.ru>
2023-02-25 20:04:06 +0300

    fail build

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -8,7 +8,6 @@ public class PostmanEchoTest{
     void shouldShowPostRequest() {
         given()
                 .baseUri("https://postman-echo.com")
-                .contentType("text/plain; charset=UTF-8")
                 .body("Тестовые данные")
                 .when()
                 .post("/post")
